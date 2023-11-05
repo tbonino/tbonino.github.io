22547fb4de2d95bdc7183ba1b4bf66666dbaf560
tbonino <tbonino@protonmail.com>
2023-11-05 16:41:45 -0500

    Changed to host code for 5th programming assignment.

---
+++ js/ProgAssign5.js
@@ -0,0 +1,55 @@
+//example code screenshotted in document
+const book = {
+    title: "Dune",
+    author: "Frank Herbert",
+    pubDateYear: 1965,
+    pubDateMonth: "August"
+  };
+  
+  let {title, author, pubDateYear, pubDateMonth} = book;
+  console.log(title, author, pubDateYear, pubDateMonth);
+
+  //create a famous quotes object literal 
+  const quoteLibrary = [
+  {
+    id: 1,
+    quote: "The buck stops here!",
+    person: "Harry S. Truman"
+  },{
+    id: 2,
+    quote: "What stands in the way becomes the way.",
+    person: "Marcus Aurelius"
+  },{
+    id: 3,
+    quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
+    person: "Dr. Martin Luther King Jr."
+  },{
+    id: 4,
+    quote: "The only thing we have to fear is fear itself.",
+    person: "Franklin D. Roosevelt"
+  },{
+    id: 5,
+    quote: "Talk softly, and carry a big stick.",
+    person: "Theodore Roosevelt"
+  },{
+    id: 6,
+    quote: "In the end, it's not the years in your life that count. It's the life in your years.",
+    person: "Abraham Lincoln"
+  },{
+    id: 7,
+    quote: "Success is not final; failure is not fatal: it is the courage to continue that counts.",
+    person: "Winston Churchill"
+  },{
+    id: 8,
+    quote: "There are no secrets to success. It is the result of preparation, hard work, and learning from failure.",
+    person: "Colin Powell"
+  },{
+    id: 9,
+    quote: "Try not to become a man of success. Rather become a man of value.",
+    person: "Albert Einstein"
+  },{
+    id: 10,
+    quote: "You miss 100% of the shots you don't take.",
+    person: "Wayne Gretzky"
+  }
+];
